df8c72d227639f6b2964b089361743c6992a8253
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2023-04-13 09:40:58 +0300

    Delete Azazel directory

--- Azazel/core/data.py
+++
@@ -1,8 +0,0 @@
-from pyrogram.types import InlineKeyboardButton, WebAppInfo
-from Azazel import CMD_HNDLR as cmds
-class Data:
-
-    text_help_menu = (
-        f"**قائمة المساعدة**\n** • البادئات** : `None`"
-    )
-    reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
